07c2d13c2a2495bb135fd676e3c934f8153cd1a3
xlotox <apodaca.brian@gmail.com>
2013-05-29 14:52:48 -0700

    test rails adding to git

---
+++ treebook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'e24e41364ad9dac52d35f1606d7a195cd6eba19537304e18511bec72fab9165699054da230816fc958e59b5276c2e8f4cee597969b72293a5d1d1fc90c79a45e'
